3e6e5e54827b7b1ce30825ab7b3912543ed917f0
jnesss <jness@fusions.ai>
2025-04-15 13:42:27 -0700

    add up to 50ms of delay to the start of handleNetworkEvent to allow for process metadata to appear in the cache.  If it is already present, there will be no delay.  If its still not there after 50ms, it will continue using only the minimal process information from the BPF program but some information might be incorrect or not consistent with the eventual process that appears.  Also changed network_uuid to be the same for both request and response, not different based on src/dst flip

--- network.go
+++ network.go
@@ -6,8 +6,9 @@ import (
 	"log"
 	"net"
 	"strings"
+	"time"
 
-	"github.com/jnesss/bpfview/outputformats" // for GenerateConnID utility function
+	"github.com/jnesss/bpfview/outputformats" // for GenerateBidirectionalConnID utility function
 	"github.com/jnesss/bpfview/types"
 )
 
@@ -51,6 +52,31 @@ func handleNetworkEvent(event *types.NetworkEvent) {
 		return
 	}
 
+	// Wait to start processing until we have processinfo
+	//  This is not my favorite design pattern
+	//  But we are reliant on the processinfo for the processUID correlation, amongst other things
+
+	var processInfo *types.ProcessInfo
+	var exists bool
+
+	// Try up to 10 times with 5ms delay (50ms total max)
+	for i := 0; i < 10; i++ {
+		processInfo, exists = GetProcessFromCache(event.Pid)
+		if exists {
+			break
+		}
+		time.Sleep(5 * time.Millisecond)
+	}
+
+	if !exists {
+		// Use minimal info if we still can't find process
+		processInfo = &types.ProcessInfo{
+			PID:  event.Pid,
+			Comm: string(bytes.TrimRight(event.Comm[:], "\x00")),
+			PPID: event.Ppid,
+		}
+	}
+
 	// Clean up process names
 	comm := string(bytes.TrimRight(event.Comm[:], "\x00"))
 	parentComm := string(bytes.TrimRight(event.ParentComm[:], "\x00"))
@@ -71,20 +97,13 @@ func handleNetworkEvent(event *types.NetworkEvent) {
 
 	globalLogger.Info("network", "%s", msg.String())
 
-	var processInfo *types.ProcessInfo
-	if info, exists := GetProcessFromCache(event.Pid); exists {
-		processInfo = info
-	} else {
-		processInfo = &types.ProcessInfo{}
-	}
-
 	if globalLogger != nil {
 		globalLogger.LogNetwork(event, processInfo)
 	}
 
 	if globalSigmaEngine != nil {
 
-		networkUID := outputformats.GenerateConnID(event.Pid, event.Ppid,
+		networkUID := outputformats.GenerateBidirectionalConnID(event.Pid, event.Ppid,
 			uint32ToNetIP(event.SrcIP),
 			uint32ToNetIP(event.DstIP),
 			event.SrcPort, event.DstPort)
