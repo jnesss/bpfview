3a05a72c0261dfd0c13637cbcd493b80e31aa806
Eric <xnih13@gmail.com>
2025-04-23 20:44:00 -0600

    Update events.go

--- types/events.go
+++ types/events.go
@@ -219,7 +219,7 @@ type UserSpaceTLSEvent struct {
 	SupportedGroups   []uint16
 	KeyShareGroups    []uint16
 	ALPNValues        []string
-	Extensions        []string
+	Extensions        []uint16
 	JA4               string
 	JA4Hash           string
 	IsQUIC            bool // Helper field to identify QUIC connections
